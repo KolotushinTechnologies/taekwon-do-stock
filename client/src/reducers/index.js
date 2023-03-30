db83b9bcd675bf1edd011a741ca6671dd2479af1
KolotushinTechnologies <118999213+DEVJDENTER@users.noreply.github.com>
2023-03-30 11:37:50 +0300

    Added Redux for Stable next work

---
+++ client/src/reducers/index.js
@@ -0,0 +1,10 @@
+// Import Engine Redux
+import { combineReducers } from "redux";
+
+// Import Reducers
+import alert from "./alert";
+
+// Export Combine Reducer
+export default combineReducers({
+    alert,
+});
